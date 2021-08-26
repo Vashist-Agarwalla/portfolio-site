5f5cfac3edfe806010f1905bb5025beaa5441bc0
Vashist-Agarwalla <vashist1202@gmail.com>
2021-08-26 22:40:00 +0530

    💄 Spacing

--- components/HomePage/Home/Home.tsx
+++ components/HomePage/Home/Home.tsx
@@ -1,18 +1,18 @@
 const Home = () => {
     return (
-        <div className='h-screen w-screen flex'>
+        <div className='h-full w-screen flex'>
             <div className='flex-1'>
                 Hello
             </div>
-            <div className='flex-3'>
-                <div className='text-white'>
+            <div className='flex-3 font-semibold mt-80 ml-32'>
+                <div className='text-white text-2xl pt-4'>
                     Hello, I am
                 </div>
-                <div className='text-secondary'>
+                <div className='text-secondary text-6xl mt-2'>
                     VASHIST AGARWALLA
                 </div>
             </div>
-            <div className='flex-3'>
+            <div className='flex-3 pr-28 my-40 w-56'>
                 <img src='Hacker-bro-1.png' />
             </div>
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,5 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
 import Home from '../components/HomePage/Home/Home'
-import styles from '../styles/Home.module.css'
 
 const HomePage: NextPage = () => {
   return (
