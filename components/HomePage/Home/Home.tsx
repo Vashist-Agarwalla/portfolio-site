216af54820f339237f7e84674a3e317c53fee971
Vashist-Agarwalla <vashist1202@gmail.com>
2021-08-26 23:11:07 +0530

    📱 Home section

--- components/HomePage/Home/Home.tsx
+++ components/HomePage/Home/Home.tsx
@@ -2,7 +2,27 @@ const Home = () => {
     return (
         <div className='h-full w-screen flex'>
             <div className='flex-1'>
-                Hello
+                <img
+                    src='Line-1.png'
+                    className='mt-32 h-60 ml-40'
+                />
+                <img
+                    src="github-480.png"
+                    className='ml-36 h-8 my-4'
+                />
+                <img
+                    src="linkedin-480.png"
+                    className='ml-36 h-8 my-4'
+                />
+                <img
+                    src="instagram-256.png"
+                    className='ml-36 h-8 my-4'
+                />
+                <img
+                    src='Line-1.png'
+                    className='mt-4 h-60 ml-40'
+                />
+                {/* Hello */}
             </div>
             <div className='flex-3 font-semibold mt-80 ml-32'>
                 <div className='text-white text-2xl pt-4'>
