68693e648f7b076a942b7678247530badced6293
Vashist-Agarwalla <vashist1202@gmail.com>
2021-08-26 16:58:01 +0530

    🎨 Add Font Poppins

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,13 +2,17 @@ module.exports = {
   purge: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
-    colors: {
-      primary: "#202124"
-    }
+    extend: {
+      colors: {
+        main: "#202124"
+      },
+      fontFamily: {
+        'display': ['Poppins']
+      }
+    },
   },
   variants: {
-    extend: {},
+    extend: { },
   },
   plugins: [],
 }
