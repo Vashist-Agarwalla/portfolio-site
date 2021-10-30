b9155840f03e69e87b40c6356d78165c599d6e6f
Vashist-Agarwalla <vashist1202@gmail.com>
2021-10-30 16:21:27 +0530

    💄 Scroll Effect

--- components/Navbar/Navbar.tsx
+++ components/Navbar/Navbar.tsx
@@ -1,9 +1,28 @@
 import Logo from "./Logo";
 import Menu from "./Menu";
+import { useEffect, useState } from "react";
 
 const Navbar = () => {
+
+    const [navCSS, setNavCSS] = useState("py-10");
+
+    const controlNavbar = () => {
+        if (window.scrollY > 50){
+            setNavCSS("py-4 bg-main filter drop-shadow-2xl lg:drop-shadow-xl")
+        } else {
+            setNavCSS("py-10")
+        }
+    }
+
+    useEffect(() => {
+        window.addEventListener('scroll', controlNavbar)
+        return () => {
+            window.removeEventListener('scroll', controlNavbar)
+        }
+    })
+
     return (
-        <nav className='fixed left-32 right-24 top-11'>
+        <nav className={`fixed left-0 right-0 pl-32 pr-24 ${navCSS}`}>
             <div className='py-1 text-white text-xl font-semibold'>
                 <div className='flex flex-row'>
                     <Logo />
