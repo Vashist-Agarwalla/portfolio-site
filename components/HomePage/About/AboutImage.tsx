f244ced803f3dac07d80c188358bd21fbed7223e
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-06 12:04:47 +0530

    💄 Image

--- components/HomePage/About/AboutImage.tsx
+++ components/HomePage/About/AboutImage.tsx
@@ -1,7 +1,10 @@
 const AboutImage = () => {
-    return(
+    return (
         <div className='flex-2'>
-
+            <img
+                src="Vashist.jpeg"
+                className='ml-32 mt-48 h-1/2 rounded-md'
+            />
         </div>
     )
 }
--- components/HomePage/About/Text.tsx
+++ components/HomePage/About/Text.tsx
@@ -1,6 +1,6 @@
 const AboutText = () => {
     return (
-        <div className='flex-3 ml-40 mr-24'>
+        <div className='flex-3 ml-40 mr-24 py-60'>
             <h1 className='text-secondary font-semibold text-3xl pb-10'>ABOUT ME</h1>
             <div className='text-white text-xl font-display font-extralight mr-12 pb-8'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
