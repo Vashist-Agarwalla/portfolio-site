b08ee59d44b87ba25221752753ccc50c57e12ec5
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-06 13:01:13 +0530

    💄 Image Spacing

--- components/HomePage/About/AboutImage.tsx
+++ components/HomePage/About/AboutImage.tsx
@@ -3,7 +3,7 @@ const AboutImage = () => {
         <div className='lg:flex-2'>
             <img
                 src="Vashist.jpeg"
-                className='mx-12 sm:mx-16 md:mx-24 lg:mx-32 mt-24 lg:mt-48 w-3/4 sm:w-2/5 lg:w-3/4 sm:h-1/4 lg:h-1/2 rounded-md'
+                className='mx-12 sm:mx-16 md:mx-24 lg:mx-32 mt-24 lg:mt-48 w-3/4 sm:w-2/5 lg:w-max sm:h-1/4 lg:h-1/2 rounded-md'
             />
         </div>
     )
