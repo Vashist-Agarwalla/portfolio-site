45638433941c469fbc50cf018403c6a3dcb52591
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-06 13:39:47 +0530

    📱 About Section

--- components/HomePage/About/Text.tsx
+++ components/HomePage/About/Text.tsx
@@ -1,6 +1,6 @@
 const AboutText = () => {
     return (
-        <div className='flex-3 ml-40 mr-24 py-60'>
+        <div className='lg:flex-3 ml-12 md:ml-24 lg:ml-40 mr-0 md:mr-24 lg:mr-24 py-16 md:pb-40 lg:py-60'>
             <h1 className='text-secondary font-semibold text-3xl pb-10'>ABOUT ME</h1>
             <div className='text-white text-xl font-display font-extralight mr-12 pb-8'>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
