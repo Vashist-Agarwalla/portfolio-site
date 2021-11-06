830300d78ffb5a7280350321e50fbd364b094f6e
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-06 09:44:51 +0530

    📱 Responsive Navbar

--- components/Navbar/Logo.tsx
+++ components/Navbar/Logo.tsx
@@ -1,9 +1,17 @@
-const Logo = () => {
-    return(
-        <div className='pl-3'>
-            Logo
-        </div>
-    )
+const Logo = (props: any) => {
+    if (props.dropdown) {
+        return (
+            <div>
+
+            </div>
+        )
+    } else {
+        return (
+            <div className='pl-3'>
+                Logo
+            </div>
+        )
+    }
 }
 
 export default Logo;
--- components/Navbar/Menu.tsx
+++ components/Navbar/Menu.tsx
@@ -1,22 +1,48 @@
-const Menu = () => {
-    return (
-        <div className='hidden lg:contents'>
-            <div className="grid grid-cols-4 gap-0 m-auto mr-0">
-                <div className="mx-12">
-                    <a href='/'>Home</a>
-                </div>
-                <div className="m-auto ">
-                    <a href='/#about'>About</a>
-                </div>
-                <div className="m-auto ">
-                    <a href='/#projects'>Projects</a>
-                </div>
-                <div className="m-auto ">
-                    <a href='/#contact'>Contact</a>
+import Logo from "./Logo";
+
+const Menu = (props: any) => {
+    if (props.dropdown) {
+        return (
+            <div className='w-full lg:hidden bg-main justify-items-center'>
+                <div className="grid grid-rows-4 gap-0 m-auto mr-0">
+                    <div className="my-0">
+                        <Logo />
+                    </div>
+                    <div className="mx-auto my-4">
+                        <a href='/'>Home</a>
+                    </div>
+                    <div className="mx-auto my-4">
+                        <a href='/#about'>About</a>
+                    </div>
+                    <div className="mx-auto my-4">
+                        <a href='/#projects'>Projects</a>
+                    </div>
+                    <div className="mx-auto my-4">
+                        <a href='/#contact'>Contact</a>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div className='hidden lg:contents'>
+                <div className="grid grid-cols-4 gap-0 m-auto mr-0">
+                    <div className="mx-12">
+                        <a href='/'>Home</a>
+                    </div>
+                    <div className="m-auto ">
+                        <a href='/#about'>About</a>
+                    </div>
+                    <div className="m-auto ">
+                        <a href='/#projects'>Projects</a>
+                    </div>
+                    <div className="m-auto ">
+                        <a href='/#contact'>Contact</a>
+                    </div>
+                </div>
+            </div>
+        )
+    }
 }
 
 export default Menu;
