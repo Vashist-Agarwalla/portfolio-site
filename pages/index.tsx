a62aad06320e051466b89f1d6c7016a8cf4ca1cc
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-08 09:42:55 +0530

    🚚 Rename Files

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from 'next'
 import Footer from '../components/Footer';
-import About from '../components/HomePage/About/About';
-import Home from '../components/HomePage/Home/Home'
-import Navbar from '../components/Navbar/Navbar';
+import About from '../components/HomePage/About/index';
+import Home from '../components/HomePage/Home/index'
+import Navbar from '../components/Navbar/index';
 
 const HomePage: NextPage = () => {
   return (
