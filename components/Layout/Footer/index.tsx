07f708a2f9a6c4927881ff6a8a9185dfa96a4d7f
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-08 10:16:04 +0530

    ✨ Hyperlink

--- components/Layout/Footer/index.tsx
+++ components/Layout/Footer/index.tsx
@@ -7,22 +7,43 @@ const Footer = () => {
             <div className="flex-1 py-6">
                 <div className="grid justify-items-center grid-cols-3 ml-72 mr-32">
                     <div>
-                        <img
-                            src="github-480.png"
-                            className='h-10 my-4'
-                        />
+                        <a
+                            href="https://github.com/Vashist-Agarwalla"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Github | Vashist-Agarwalla"
+                        >
+                            <img
+                                src="github-480.png"
+                                className='h-10 my-4'
+                            />
+                        </a>
                     </div>
                     <div>
-                        <img
-                            src="linkedin-480.png"
-                            className='h-10 my-4'
-                        />
+                        <a
+                            href="https://www.linkedin.com/in/vashist-agarwalla/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="LinkedIn | Vashist-Agarwalla"
+                        >
+                            <img
+                                src="linkedin-480.png"
+                                className='h-10 my-4'
+                            />
+                        </a>
                     </div>
                     <div>
-                        <img
-                            src="instagram-256.png"
-                            className='h-10 my-4'
-                        />
+                        <a
+                            href="https://www.instagram.com/vashist_agarwalla/"
+                            target="_blank"
+                            rel="noopener noreferrer"
+                            title="Instagram | Vashist-Agarwalla"
+                        >
+                            <img
+                                src="instagram-256.png"
+                                className='h-10 my-4'
+                            />
+                        </a>
                     </div>
                 </div>
             </div>
