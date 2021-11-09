041cad2f27171fe5b6d374c82143a983b4e5e94d
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-09 10:09:18 +0530

    ✨ Passing Props

--- components/HomePage/Skills/Skill.tsx
+++ components/HomePage/Skills/Skill.tsx
@@ -1,14 +1,14 @@
-const Skill = ({ image }: any) => {
+const Skill = ({ image, name }: any) => {
     return (
-        <div className="p-5">
+        <div className="m-5 filter drop-shadow-lg">
             <div className="bg-footer rounded-lg h-28 text-center">
-                <div className="py-3">
+                <div className="py-3 px-14">
                     <img
                         src={image}
                         className="h-14 mx-auto"
                     />
                 </div>
-                JavaScript
+                {name}
             </div>
         </div>
     )
--- components/HomePage/Skills/index.tsx
+++ components/HomePage/Skills/index.tsx
@@ -1,20 +1,57 @@
 import Skill from "./Skill"
 
 const Skills = () => {
-    const data = [{
-        image: '/Skills/ts.svg'
-    }]
+    const data = [
+        {
+            image: '/Skills/js.svg',
+            name: 'JavaScript'
+        },
+        {
+            image: '/Skills/ts.svg',
+            name: 'TypeScript'
+        },
+        {
+            image: '/Skills/node.svg',
+            name: 'Node JS'
+        },
+        {
+            image: '/Skills/express.svg',
+            name: 'Express'
+        },
+        {
+            image: '/Skills/nextjs.svg',
+            name: 'NextJS'
+        },
+        {
+            image: '/Skills/mongodb.svg',
+            name: 'MongoDB'
+        },
+        {
+            image: '/Skills/react.svg',
+            name: 'React'
+        },
+        {
+            image: '/Skills/Adobe_Photoshop_CC.svg',
+            name: 'Abobe Photoshop CC'
+        },
+        {
+            image: '/Skills/Google_Cloud_Platform.svg',
+            name: 'Google Cloud Platform'
+        },
+        {
+            image: '/Skills/Amazon_Web_Services.svg',
+            name: 'Amazon Web Services'
+        },
+    ]
     return (
         <div className="py-12">
             <div className="text-secondary font-semibold text-center text-3xl">
                 SKILLS
             </div>
             <div className="grid grid-cols-5 mx-16">
-                <Skill image={data[0].image} />
-                <Skill image='/Skills/Adobe_Photoshop_CC.svg' />
-                <Skill image='/Skills/Adobe_Photoshop_CC.svg' />
-                <Skill image='/Skills/Adobe_Photoshop_CC.svg' />
-                <Skill image='/Skills/Adobe_Photoshop_CC.svg' />
+                {data.map((d, i) => (
+                    <Skill image={d.image} name={d.name} />
+                ))}
             </div>
         </div>
     )
