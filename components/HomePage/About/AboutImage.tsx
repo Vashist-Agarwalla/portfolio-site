3a64f166610e5efbd2b01bc87b4c3e48b86d784b
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-09 09:26:34 +0530

    👽️ Remove Dynamic Content

--- components/HomePage/About/AboutImage.tsx
+++ components/HomePage/About/AboutImage.tsx
@@ -2,7 +2,7 @@ const AboutImage = () => {
     return (
         <div className='lg:flex-2'>
             <img
-                src="Vashist.jpeg"
+                src="https://s3.ap-south-1.amazonaws.com/me.vashist/Vashist-Agarwalla.jpeg"
                 className='mx-12 sm:mx-16 md:mx-24 lg:mx-32 mt-24 lg:mt-48 w-3/4 sm:w-2/5 lg:w-max sm:h-1/4 lg:h-1/2 rounded-md'
             />
         </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,23 +1,23 @@
 import About from '../components/HomePage/About';
 import Home from '../components/HomePage/Home';
-import axios from 'axios';
+// import axios from 'axios';
 
 const HomePage = ({ about }: any) => {
   return (
     <div className='bg-main'>
       <Home />
-      <About about={about} />
+      <About />
     </div>
   )
 }
 
-export const getServerSideProps = async () => {
-  const data = await axios.get(`${process.env.DOMAIN}/api/about-section`);
-  return {
-    props: {
-      about: data.data
-    }
-  }
-}
+// export const getServerSideProps = async () => {
+//   const data = await axios.get(`${process.env.DOMAIN}/api/about-section`);
+//   return {
+//     props: {
+//       about: data.data
+//     }
+//   }
+// }
 
 export default HomePage;
