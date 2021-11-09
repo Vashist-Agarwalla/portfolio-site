15ca04d84eeb046f5494df4c7e645d57c7ce7a28
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-09 12:04:03 +0530

    ⚡️ View More Skills

--- components/HomePage/Skills/index.tsx
+++ components/HomePage/Skills/index.tsx
@@ -1,6 +1,8 @@
+import { useState } from "react"
 import Skill from "./Skill"
 
 const Skills = () => {
+    const [more, setMore] = useState(false)
     const data = [
         {
             image: '/Skills/js.svg',
@@ -40,18 +42,49 @@ const Skills = () => {
         },
         {
             image: '/Skills/Amazon_Web_Services.svg',
-            name: 'Amazon Web Services'
+            name: 'Amazon Web Services',
+        },
+        {
+            image: '/Skills/HTML5.svg',
+            name: 'HTML 5'
+        },
+        {
+            image: '/Skills/CSS3.svg',
+            name: 'CSS 3',
+        },
+        {
+            image: '/Skills/C++.svg',
+            name: 'C++'
+        },
+        {
+            image: '/Skills/Python.svg',
+            name: 'Python',
         },
     ]
+    const showMore = () => {
+        setMore(!more);
+    }
     return (
         <div className="py-12">
-            <div className="text-secondary font-semibold text-center text-3xl">
+            <div className="text-secondary font-semibold text-center text-3xl pb-5">
                 SKILLS
             </div>
-            <div className="grid grid-cols-5 mx-16">
-                {data.map((d, i) => (
-                    <Skill image={d.image} name={d.name} />
-                ))}
+            <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-5 mx-16">
+                {data.map((d, i) => {
+                    if (more)
+                        return (
+                            <Skill key={i} image={d.image} name={d.name} />
+                        )
+                    else if (i < 10)
+                        return (
+                            <Skill key={i} image={d.image} name={d.name} />
+                        )
+                })}
+            </div>
+            <div className="mt-5 mx-auto w-36 border border-secondary rounded text-center text-white hover:text-black bg-other hover:bg-secondary">
+                <button className="h-10" onClick={showMore}>
+                    View More
+                </button>
             </div>
         </div>
     )
