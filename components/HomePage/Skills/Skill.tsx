f073c30a439eda735bef640c8de8f0234579c6c2
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-10 10:29:24 +0530

    💄 Skill UI Fix

--- components/HomePage/Skills/Skill.tsx
+++ components/HomePage/Skills/Skill.tsx
@@ -1,8 +1,8 @@
 const Skill = ({ image, name }: any) => {
     return (
         <div className="m-5 filter drop-shadow-lg font-display">
-            <div className="bg-other hover:bg-secondary rounded-lg h-28 text-center text-white hover:text-black">
-                <div className="py-3 px-14">
+            <div className="bg-other hover:bg-secondary rounded-lg h-28 lg:h-32 xl:h-28 text-center text-white hover:text-black">
+                <div className="py-3 px-14 lg:px-10 xl:px-14">
                     <img
                         src={image}
                         alt={name}
