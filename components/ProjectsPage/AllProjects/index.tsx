2536a2a370f4cc4eb5564bd7606e123e5b39bdb2
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-10 18:44:42 +0530

    📱 Projects Page

--- components/ProjectsPage/AllProjects/index.tsx
+++ components/ProjectsPage/AllProjects/index.tsx
@@ -3,8 +3,10 @@ import Project from "./Project";
 const AllProjects = () => {
     return (
         <div className="my-16">
-            <h1 className="text-secondary text-center text-5xl font-semibold">PROJECTS</h1>
-            <div className="grid grid-cols-2 mt-10 mx-16">
+            <h1 className="text-secondary text-center text-5xl lg:text-5xl font-semibold">
+                PROJECTS
+            </h1>
+            <div className="grid grid-cols-1 lg:grid-cols-2 mt-10 mx-10 sm:mx-24 lg:mx-16">
                 <Project />
                 <Project />
                 <Project />
