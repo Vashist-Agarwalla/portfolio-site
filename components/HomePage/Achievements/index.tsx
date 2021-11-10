f9b47028bc3d4be013313bf837aebbe1892fa7dc
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-10 09:43:47 +0530

    ⚡️ Passing Achievements Props

--- components/HomePage/Achievements/index.tsx
+++ components/HomePage/Achievements/index.tsx
@@ -1,12 +1,29 @@
 import Achievement from "./Achievement";
 
 const Achievements = () => {
+    const data = [
+        {
+            name: "Hacka-demic",
+            image: "/Achievements/HackInTheNorth_2020.svg",
+            date: "20th December 2020",
+            description: "Qualified in the final round of Hack-demic Hackathon, hosted by Hack In The North, IIIT-A"
+        },
+        {
+            name: "GeekXtream 1.0",
+            image: "/Achievements/GFGSRM.jpg",
+            date: "14th December 2020",
+            description: "Runner up in GeekXtream 1.0 Hackathon, Hosted by GFG SRM"
+        }
+    ]
     return (
         <div className="py-12">
             <h1 className="text-secondary font-semibold text-3xl text-center">ACHIEVEMENTS</h1>
             <div className="grid grid-cols-2 py-5 mx-16">
-                <Achievement />
-                <Achievement />
+                {data.map((d: any, i: any) => {
+                    return (
+                        <Achievement key={i} data={d} />
+                    )
+                })}
             </div>
         </div>
     )
