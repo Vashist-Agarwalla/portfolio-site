dc24dd764e60123b59d605c73f5576a3334b192e
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-10 19:03:13 +0530

    ⚡️ Passing Props

--- components/ProjectsPage/AllProjects/Project.tsx
+++ components/ProjectsPage/AllProjects/Project.tsx
@@ -1,9 +1,10 @@
-const Project = () => {
+const Project = ({ data }: any) => {
     return (
         <div className="py-6 lg:py-10 mx-0 lg:mx-20 font-display">
             <div className="transition duration-500 ease-in-out bg-other hover:bg-secondary filter drop-shadow-lg hover:drop-shadow-2xl rounded-lg text-secondary transform hover:scale-110">
-                <img 
-                    src="https://s3.ap-south-1.amazonaws.com/me.vashist/Projects/0001-TechAnalogy-Home.png"
+                <img
+                    src={data.image}
+                    alt={data.name}
                     className="object-fill rounded-lg h-full w-full"
                 />
             </div>
