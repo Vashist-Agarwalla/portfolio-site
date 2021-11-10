1530182895324be8ebdbe77d030c37142e084e66
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-10 19:20:15 +0530

    ✨ Projects Page API

---
+++ pages/api/projects-page.ts
@@ -0,0 +1,26 @@
+import nc from "next-connect";
+import { NextApiRequest, NextApiResponse } from "next";
+import { connectToDatabase } from "../../util/mongo";
+
+function sortByProperty(property: any) {
+    return function (a: any, b: any) {
+        if (a[property] > b[property])
+            return 1;
+        else if (a[property] < b[property])
+            return -1;
+        return 0;
+    }
+}
+
+const handler = nc()
+    .get(async (req: NextApiRequest, res: NextApiResponse) => {
+        const { db } = await connectToDatabase();
+        const projects = await db
+            .collection("Projects")
+            .find({})
+            .toArray()
+        projects.sort(sortByProperty('pid'))
+        res.json([projects])
+    })
+
+export default handler;
--- pages/projects/index.tsx
+++ pages/projects/index.tsx
@@ -1,11 +1,21 @@
 import AllProjects from "../../components/ProjectsPage/AllProjects";
+import axios from "axios";
 
-const ProjectsPage = () => {
+const ProjectsPage = ({ projects }: any) => {
     return (
         <div className="pt-28">
-            <AllProjects />
+            <AllProjects data={projects} />
         </div>
     )
+}
+
+export const getServerSideProps = async () => {
+    const data = await axios.get(`${process.env.DOMAIN}/api/projects-page`);
+    return {
+        props: {
+            projects: data.data[0],
+        }
+    }
 }
 
 export default ProjectsPage;
