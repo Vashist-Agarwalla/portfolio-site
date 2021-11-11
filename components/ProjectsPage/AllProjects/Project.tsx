f35042dec5847ab579095b1249abee766e262f87
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-11 13:38:59 +0530

    ⚡️ Link on Card

--- components/ProjectsPage/AllProjects/Project.tsx
+++ components/ProjectsPage/AllProjects/Project.tsx
@@ -1,14 +1,19 @@
+import Link from "next/link";
+
 const Project = ({ data }: any) => {
+    const link = `/projects/${data.slug}`
     return (
-        <div className="py-6 lg:py-10 mx-0 lg:mx-20 font-display">
-            <div className="transition duration-500 ease-in-out bg-other hover:bg-secondary filter drop-shadow-lg hover:drop-shadow-2xl rounded-lg text-secondary transform hover:scale-110">
-                <img
-                    src={data.image}
-                    alt={data.name}
-                    className="object-fill rounded-lg h-full w-full"
-                />
+        <Link href={link}>
+            <div className="py-6 lg:py-10 mx-0 lg:mx-20 font-display">
+                <div className="transition duration-500 ease-in-out bg-other hover:bg-secondary filter drop-shadow-lg hover:drop-shadow-2xl rounded-lg text-secondary transform hover:scale-110">
+                    <img
+                        src={data.image}
+                        alt={data.name}
+                        className="object-fill rounded-lg h-full w-full"
+                    />
+                </div>
             </div>
-        </div>
+        </Link>
     )
 }
 
