f74286adf523f54d1520b5f2e66f2230d3d454b1
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-11 21:13:41 +0530

    📱 Know More Section

--- components/KnowMorePage/WorkExprience/Card.tsx
+++ components/KnowMorePage/WorkExprience/Card.tsx
@@ -1,7 +1,7 @@
 const Card = () => {
     return (
-        <div className="bg-other rounded-xl filter drop-shadow-lg my-16 px-8 py-6">
-            <h1 className="text-2xl font-semibold text-secondary uppercase">Tech Analogy</h1>
+        <div className="bg-secondary rounded-xl filter drop-shadow-lg my-8 lg:my-16 px-8 py-6">
+            <h1 className="text-2xl font-bold text-black uppercase">Tech Analogy</h1>
             <p className="text-white pt-2 italic font-display text-md">Software Development Executive</p>
             <p className="text-white py-2 italic font-display text-md">Apr 2021 - Present</p>
             <ul className="list-disc list-inside text-white py-2 font-display text-md">
--- components/KnowMorePage/WorkExprience/index.tsx
+++ components/KnowMorePage/WorkExprience/index.tsx
@@ -6,8 +6,8 @@ const WorkExperience = () => {
             <h1 className="my-10 text-secondary text-center text-5xl lg:text-5xl font-semibold">
                 WORK EXPERIENCE
             </h1>
-            <div className="flex flex-row-reverse my-14 mx-14">
-                <div className="flex-1 mx-20 py-5">
+            <div className="md:flex flex-row-reverse my-14 mx-2 md:mx-10 lg:mx-14">
+                <div className="flex-1 mx-8 lg:mx-20 md:pt-0 lg:pt-5 py-5">
                     <div className="transition duration-500 ease-in-out bg-other my-10 py-4 rounded-md text-center text-secondary uppercase font-semibold text-xl transform hover:scale-110">
                         Tech Analogy
                     </div>
@@ -15,7 +15,7 @@ const WorkExperience = () => {
                         Tech Analogy
                     </div>
                 </div>
-                <div className="flex-1 mx-20">
+                <div className="flex-1 mx-8 lg:mx-20">
                     <Card />
                 </div>
             </div>
