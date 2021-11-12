7e0307697dd32227c98643df8240be03eb3c98cf
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-12 09:48:15 +0530

    ⚡️ Passing API Data

--- components/KnowMorePage/WorkExprience/Card.tsx
+++ components/KnowMorePage/WorkExprience/Card.tsx
@@ -1,13 +1,15 @@
-const Card = () => {
+const Card = ({ data }: any) => {
     return (
         <div className="bg-secondary rounded-xl filter drop-shadow-lg my-8 lg:my-16 px-8 py-6">
-            <h1 className="text-2xl font-bold text-black uppercase">Tech Analogy</h1>
-            <p className="text-white pt-2 italic font-display text-md">Software Development Executive</p>
-            <p className="text-white py-2 italic font-display text-md">Apr 2021 - Present</p>
+            <h1 className="text-2xl font-bold text-black uppercase">{data.name}</h1>
+            <p className="text-white pt-2 italic font-display text-md">{data.position}</p>
+            <p className="text-white py-2 italic font-display text-md">{data.duration}</p>
             <ul className="list-disc list-inside text-white py-2 font-display text-md">
-                <li>Hi</li>
-                <li>Hi</li>
-                <li>Hi</li>
+                {data.tasks.map((d: any, i: any) => {
+                    return (
+                        <li key={i}>{d}</li>
+                    )
+                })}
             </ul>
         </div>
     )
--- pages/knowMore.tsx
+++ pages/knowMore.tsx
@@ -1,11 +1,22 @@
 import WorkExperience from "../components/KnowMorePage/WorkExprience";
+import { knowMorePage } from "./api/know-more-page";
+import { GetStaticProps } from "next";
 
-const KnowMorePage = () => {
+const KnowMorePage = ({ experience }: any) => {
     return (
         <div className="pt-28">
-            <WorkExperience  />
+            <WorkExperience data={experience} />
         </div>
     )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+    const data = await knowMorePage()
+    return {
+        props: {
+            experience: data.experience,
+        }
+    }
 }
 
 export default KnowMorePage;
