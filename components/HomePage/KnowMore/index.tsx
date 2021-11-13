65955601468ea09bf90fe0972979e3aa9fb4ec86
Vashist-Agarwalla <vashist1202@gmail.com>
2021-11-13 14:26:38 +0530

    ⚡️ Redirecting to Know More

--- components/HomePage/KnowMore/index.tsx
+++ components/HomePage/KnowMore/index.tsx
@@ -3,9 +3,9 @@ import Link from 'next/link';
 const KnowMore = () => {
     return (
         <div className="m-10">
-            <div className='lg:flex'>
-                <div className='lg:flex-3'>
-                    <Link href='/knowMore'>
+            <Link href='/knowMore'>
+                <div className='lg:flex'>
+                    <div className='lg:flex-3'>
                         <div className='md:ml-24 lg:ml-32 lg:mt-32'>
                             <h1 className="text-secondary text-6xl font-semibold">Tap to</h1>
                             <h1 className="text-secondary text-6xl font-semibold uppercase">Know More?</h1>
@@ -13,15 +13,16 @@ const KnowMore = () => {
                                 <img src="/arrows.svg" className="h-16 animate-moveRight" />
                             </div>
                         </div>
-                    </Link>
+                    </div>
+                    <div className='lg:flex-2 lg:pr-16 xl:pr-28 md:mx-16 lg:mx-4 md:w-10/12 xl:w-56'>
+                        <img
+                            src='Know-More.png'
+                            alt="Image"
+                        />
+                    </div>
                 </div>
-                <div className='lg:flex-2 lg:pr-16 xl:pr-28 md:mx-16 lg:mx-4 md:w-10/12 xl:w-56'>
-                    <img
-                        src='Know-More.png'
-                        alt="Image"
-                    />
-                </div>
-            </div>
+            </Link>
+
         </div>
     )
 }
